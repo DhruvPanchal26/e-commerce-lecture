0b928a0642547b9bcac5776dcdba61081f825c2a
Baskar022 <162339221+Baskar022@users.noreply.github.com>
2025-01-18 10:49:24 +0530

    Update new-collection.js

--- src/components/new-collection/new-collection.js
+++ src/components/new-collection/new-collection.js
@@ -6,37 +6,42 @@ const NewCollection = () => {
     {
       image:
         "https://images.unsplash.com/photo-1524095731963-b4e38d1b3329?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "title 1",
-      subTitle: "sub_title",
-      price: 1,
+      title: "Jersey",
+      subTitle: "Authentic",
+      price: 200,
+      "id" : "id1"
     },
     {
       image:
         "https://images.unsplash.com/photo-1524095731963-b4e38d1b3329?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "title 2",
-      subTitle: "sub_title",
-      price: 1,
+      title: "Tracks",
+      subTitle: "Sweat proof",
+      price: 400,
+      "id" : "id2"
     },
     {
       image:
         "https://images.unsplash.com/photo-1524095731963-b4e38d1b3329?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "title 3",
-      subTitle: "sub_title",
-      price: 1,
+      title: "Shirt",
+      subTitle: "Cotton",
+      price: 150,
+      "id" : "id3"
     },
     {
       image:
         "https://images.unsplash.com/photo-1524095731963-b4e38d1b3329?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "title 4",
-      subTitle: "sub_title",
-      price: 1,
+      title: "Pants",
+      subTitle: "Cotton",
+      price: 200,
+      "id" : "id4"
     },
     {
       image:
         "https://images.unsplash.com/photo-1524095731963-b4e38d1b3329?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "title 5",
-      subTitle: "sub_title",
-      price: 1,
+      title: "Shoes",
+      subTitle: "All types",
+      price: 200,
+      "id" : "id5"
     },
   ];
   return (
@@ -46,6 +51,7 @@ const NewCollection = () => {
         {cards_info.map((item, index) => {
           return (
             <Cards
+              id={item.id}
               key={index}
               image={item.image}
               title={item.title}
